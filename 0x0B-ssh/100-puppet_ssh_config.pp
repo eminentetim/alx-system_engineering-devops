e57b9ac67f3be41fd07abc8dee39a5332a70b479
etimeminent@gmail.com <etimeminent@gmail.com>
2024-01-23 11:13:08 -0800

    redirection

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,12 @@
-# Seting up my client config file
-include stdlib
-
-file_line { 'Turn off passwd auth':
-  ensure => present,
+#client SSH configuration file to connect to server without typing a password.
+file { 'Turn 
+off passwd auth':
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
-  replace => true,
 }
-
-file_line { 'Delare identity file':
-  ensure => present,
+file { 'Declare identity file':
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '     IdentityFile ~/.ssh/school',
-  replace => true,
-}
+  line   => '    IdentityFile ~/.ssh/school',
+  }
